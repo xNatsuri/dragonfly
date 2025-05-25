befc96d19b6111cb496a2439055d2ed8b83911ea
Jon <xnatsuri@protonmail.com>
2025-05-24 22:41:05 -0700

    redundant

--- server/world/mcdb/leveldat/data.go
+++ server/world/mcdb/leveldat/data.go
@@ -148,6 +148,7 @@ type Data struct {
 	TNTExplosionDropDecay          bool           `nbt:"tntexplosiondropdecay"`
 	HasUncompleteWorldFileOnDisk   bool           `nbt:"HasUncompleteWorldFileOnDisk"`
 	PlayerHasDied                  bool           `nbt:"PlayerHasDied"`
+	LocatorBar                     bool           `nbt:"locatorbar"`
 }
 
 // FillDefault fills out d with all the default level.dat values.
